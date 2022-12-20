a7dc1428aeeec29c118218e51824f4d4c908ef14
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-19 20:29:20 -0600

    Clase 67 completa

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Listview2Screen extends StatelessWidget {
    
-   final options = const ['Megaman', 'Metañ Geaar', 'Pokemon', 'Goku'];
+   final options = const ['Megaman', 'Metal Geaar', 'Pokemon', 'Goku'];
   const Listview2Screen({Key? key}) : super(key: key);
   
   @override
@@ -10,12 +10,18 @@ class Listview2Screen extends StatelessWidget {
     return  Scaffold(
       appBar: AppBar(
         title: const Text("Listview Tipo 2"),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
       ),
       body:ListView.separated(
        itemCount: options.length, //Cuantas piezas tiene el arreglo de opciones
        itemBuilder: (context, i) => ListTile(
               title: Text(options[i]),
-              trailing: const Icon(Icons.arrow_forward_ios_outlined),
+              trailing: const Icon(Icons.arrow_forward_ios_outlined, color: Colors.indigo,),
+              onTap: () {
+                final game =options[i];
+                print(game);
+              },
             ),
        separatorBuilder: (_, __) => const Divider(), 
        )
