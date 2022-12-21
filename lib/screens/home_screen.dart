00c2cb966b2a5caef1343fac2741fc6a90a6e436
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-20 19:08:50 -0600

    69. Diseño del HomeScreen completa

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,10 +6,22 @@ class HomeScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes en Flutter'),
+        elevation: 0,
       ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          leading: const Icon(Icons.access_time_filled_outlined),
+          title: const Text('Nombre de ruta'),
+          onTap: () {
+            
+          },
+        ), 
+        separatorBuilder: (_, __) => const Divider(), 
+        itemCount: 100
+        )
     );
   }
 }
