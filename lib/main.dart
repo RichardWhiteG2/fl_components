7c7fa527ffc8abe9f47d2292145f4beed5ba12bc
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-20 18:56:32 -0600

    Clase 68 completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 
-import 'package:fl_components/screens/alert_screen.dart';
-import 'package:fl_components/screens/card_screen.dart';
-import 'package:fl_components/screens/listview1_screen.dart';
+
 import 'package:flutter/material.dart';
 
-import 'package:fl_components/screens/listview2_screen.dart';
+import 'package:fl_components/screens/screens.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -16,12 +15,14 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: const CardScreen(),
+      //home: const CardScreen(),
+      initialRoute: 'home',
       routes: {
+        'home'     : (BuildContext context ) => const HomeScreen(),
         'listview1': (BuildContext context ) => const Listview1Screen(),
-        'listview1': (BuildContext context ) => const Listview2Screen(),
-        'alert': (BuildContext context ) => const AlertScreen(),
-        'card': (BuildContext context ) => const CardScreen(),
+        'listview2': (BuildContext context ) => const Listview2Screen(),
+        'alert'    : (BuildContext context ) => const AlertScreen(),
+        'card'     : (BuildContext context ) => const CardScreen(),
       }
     );
   }
