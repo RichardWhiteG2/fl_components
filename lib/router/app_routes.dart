b65a4248225052355d876b471ffab0df163759ca
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-23 10:52:52 -0600

    72. Modelo y Listado de Rutas COMPLETO

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -2,11 +2,21 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:fl_components/models/models.dart';
 import '../screens/screens.dart';
 
 class AppRoutes{
 
   static const initialRoute ='home';
+  static final menuOptions =<MenuOption>[
+    //TODO:B orrar home
+    MenuOption(route: 'home', name: 'Home Screen', screen: const HomeScreen(), icon: Icons.home_max_sharp),
+    MenuOption(route: 'listview1', name: 'Listview tipo 1', screen: const Listview1Screen(), icon: Icons.list_alt),
+    MenuOption(route: 'listview2', name: 'Listview tipo 2', screen: const Listview2Screen(), icon: Icons.list),
+    MenuOption(route: 'alert', name: 'Alertas', screen: const AlertScreen(), icon: Icons.add_alert_outlined),
+    MenuOption(route: 'card', name: 'Tarjetas - Cars', screen: const CardScreen(), icon: Icons.credit_card)
+  ];
+
   static Map<String, Widget Function(BuildContext)> routes = {
 
     'home'     : (BuildContext context ) => const HomeScreen(),
