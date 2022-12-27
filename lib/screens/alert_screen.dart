d0060202542832ca47f611727682af8a1e97dbaf
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-26 19:01:39 -0600

    82 .- Mostrar una alerta Diseño COMPLETO

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -4,6 +4,48 @@ class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
   
+
+  void displayDialog(BuildContext context){
+    
+    showDialog(
+      //barrierDismissible hace que se cierre el dialog tocando la sombra
+      barrierDismissible: false,
+      context: context, 
+      builder: (context) {
+
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Titulo'),
+          shape: RoundedRectangleBorder( borderRadius: BorderRadiusDirectional.circular(10) ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox( height: 10 ),
+              FlutterLogo( size: 100 )
+            ],
+          ),
+          actions: [
+
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar')
+            ),
+
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok')
+            ),
+
+          ],
+        );
+        
+      }
+      );
+
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -13,9 +55,7 @@ class AlertScreen extends StatelessWidget {
             padding:  EdgeInsets.all(8.0),
             child:  Text('Mostrar Alerta', style: TextStyle(fontSize: 16)),
           ),
-          onPressed: () {
-
-          } 
+          onPressed: () => displayDialog (context)
         )
       ),
       floatingActionButton: FloatingActionButton(
