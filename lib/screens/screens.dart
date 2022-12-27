b64290c6e80ed8e42b7ead124297b94b24580eea
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-26 19:27:30 -0600

    84.- Circle Avatar

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,7 +1,15 @@
 
 
+
+
+
 export 'package:fl_components/screens/alert_screen.dart';
 export 'package:fl_components/screens/card_screen.dart';
 export 'package:fl_components/screens/home_screen.dart';
 export 'package:fl_components/screens/listview1_screen.dart';
 export 'package:fl_components/screens/listview2_screen.dart';
+export 'package:fl_components/screens/avatar_screen.dart';
+
+
+
+
