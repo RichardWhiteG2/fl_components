9711e7dbeddc1670ab52526fdc9c25795dc4be9b
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-26 18:07:30 -0600

    80 Argumentos a los Widgets COMPLETO

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,14 @@ import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  final String imageUrl;
+  final String? name; //Puede si o no enviar el nombre
+
+  const CustomCardType2({
+    super.key, 
+    required this.imageUrl, 
+    this.name
+    });
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +25,23 @@ class CustomCardType2 extends StatelessWidget {
           /*Image(
             image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/8/86/Landscape_mountain.jpg')
           )*/
-          const FadeInImage(
-            image:  NetworkImage('https://upload.wikimedia.org/wikipedia/commons/8/86/Landscape_mountain.jpg'), 
-            placeholder:  AssetImage('assets/jar-loading.gif'),
+            FadeInImage(
+            image:  NetworkImage(imageUrl ), 
+            placeholder:  const AssetImage('assets/jar-loading.gif'),
             width: double.infinity,//Parametro para indicar tomar todo el ancho.
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration:  Duration(milliseconds: 4200),
+            fadeInDuration:  const Duration(milliseconds: 4200),
           ),
-          Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(right: 20,top: 20, bottom: 20),
-            child: const Text('Un hermoso paisaje')
-            )
+          
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20,top: 20, bottom: 20),
+              //Se pone ?? porque puede que no le pasen nada, si es el caso se toma el string de  Hermoso paisaje
+            child: Text(name ?? 'Hermoso paisaje') 
+
+            
+              )
         ],
       ),
     );
